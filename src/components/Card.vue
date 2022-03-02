40406fe5f574776ca84928ef3e2dba818a50cdb4
wchr-aun <wchr.aun@gmail.com>
2022-03-02 18:09:00 +0000

    style(card): to change the position of the book img

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,59 +1,57 @@
 <template>
   <div>
     <v-card class="mx-auto">
-      <v-img
-        class="white--text align-end grey darken-2"
-        height="200px"
-        :src="imageUrl"
-        contain
-      >
-        <div style="background-color: rgb(55, 71, 79, 0.7)">
+      <v-row>
+        <v-col cols="2">
+          <v-img height="100%" :src="imageUrl" contain> </v-img>
+        </v-col>
+        <v-col>
           <v-card-title>{{ title }} ({{ date }})</v-card-title>
           <v-card-subtitle>
             by
             <span class="font-weight-bold">{{ author }}</span>
           </v-card-subtitle>
-        </div>
-      </v-img>
+          <v-divider></v-divider>
+          <v-card-subtitle class="pb-0">
+            {{ description }}
+            <span v-if="desc.length > 200">
+              <span
+                class="font-weight-bold text-caption indigo--text"
+                style="cursor: pointer"
+                v-if="!showMore"
+                @click="showMore = !showMore"
+              >
+                Show More
+              </span>
+              <span
+                class="font-weight-bold text-caption indigo--text"
+                style="cursor: pointer"
+                v-else
+                @click="showMore = !showMore"
+              >
+                Show Less
+              </span>
+            </span>
+          </v-card-subtitle>
 
-      <v-card-subtitle class="pb-0">
-        {{ description }}
-        <span v-if="desc.length > 200">
-          <span
-            class="font-weight-bold text-caption indigo--text"
-            style="cursor: pointer"
-            v-if="!showMore"
-            @click="showMore = !showMore"
-          >
-            Show More
-          </span>
-          <span
-            class="font-weight-bold text-caption indigo--text"
-            style="cursor: pointer"
-            v-else
-            @click="showMore = !showMore"
-          >
-            Show Less
-          </span>
-        </span>
-      </v-card-subtitle>
-
-      <v-card-actions>
-        <Details
-          :title="title"
-          :publicationYear="date"
-          :author="author"
-          :isbn="isbn"
-          :reviewCount="reviewCount"
-          :rating="rating"
-          :url="url"
-        />
-        <Review
-          :bookTitle="title"
-          :reviews="[]"
-          @fetch="fetchReviews('', 0, '')"
-        />
-      </v-card-actions>
+          <v-card-actions>
+            <Details
+              :title="title"
+              :publicationYear="date"
+              :author="author"
+              :isbn="isbn"
+              :reviewCount="reviewCount"
+              :rating="rating"
+              :url="url"
+            />
+            <Review
+              :bookTitle="title"
+              :reviews="[]"
+              @fetch="fetchReviews('', 0, '')"
+            />
+          </v-card-actions>
+        </v-col>
+      </v-row>
     </v-card>
   </div>
 </template>
