af2c4496873d9ab8fe8b34b0c94a73a4131eefe6
wchr-aun <wchr.aun@gmail.com>
2022-03-23 14:01:35 +0000

    feat(axios): add server close error

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,35 +5,32 @@ const instance = axios.create();
 
 instance.interceptors.request.use(
   (config) => {
-    {
-      // console.info("*", config);
-      store.dispatch("setLoading", true);
-    }
+    store.dispatch("setLoading", true);
     return config;
   },
   (error) => {
-    {
-      store.dispatch("setError", "Request Error");
-    }
+    store.dispatch("setError", "Request Error");
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
   (config) => {
-    {
-      // console.info("*", config);
-      store.dispatch("setLoading", false);
-    }
+    store.dispatch("setLoading", false);
     if (config.data?.error_message)
       store.dispatch("setError", config.data.error_message);
     return config;
   },
   (error) => {
-    console.log(error);
-    {
-      store.dispatch("setLoading", false);
-      store.dispatch("setError", "Network Error");
+    store.dispatch("setLoading", false);
+    if (error.response?.status) {
+      console.log(error.response);
+      store.dispatch("setError", error.response.data);
+    } else {
+      store.dispatch(
+        "setError",
+        "Server is currently closed. Please contact us to reopen the server"
+      );
     }
     return Promise.reject(error);
   }
